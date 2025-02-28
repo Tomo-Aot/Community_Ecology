7a11a5befe57f240870b3ee0697f7daf96fde760
Tomo-Aot <vespaanalis833@gmail.com>
2025-02-28 22:52:44 +0900

    Update Redundancy_Analysis.R

--- Redundancy_Analysis.R
+++ Redundancy_Analysis.R
@@ -1,2 +1,20 @@
 # 冗長性分析 (Redundancy Analysis; RDA)
+# packages
+library(tidyverse)
+library(vegan)
+library(emmeans)
+
+# vector of environmental variables
+env_vector = function(PCA_Results){
+  env_vector = 
+    vegan::scores(PCA_Results, display = "bp")
+  
+  colnames(env_vector) = c("xend", "yend")  # 環境ベクトルの列名を設定
+  
+  env_vector = env_vector |> 
+    as_tibble() |> 
+    mutate(factor = rownames(env_vector))
+  
+  return(env_vector)
+}
 
