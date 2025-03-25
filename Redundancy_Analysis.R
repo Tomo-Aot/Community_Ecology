ffeeb81d578291116e6fe9cec62e846d23c95d5c
Tomo-Aot <vespaanalis833@gmail.com>
2025-03-26 01:09:01 +0900

    Update Redundancy_Analysis.R

--- Redundancy_Analysis.R
+++ Redundancy_Analysis.R
@@ -1,15 +1,31 @@
-# 冗長性分析 (Redundancy Analysis; RDA)
-# packages
+# Redundancy Analysis
 library(tidyverse)
 library(vegan)
-library(emmeans)
+library(ggtext)
+library(showtext)
+library(ggpubr)
 
-# vector of environmental variables
-env_vector = function(PCA_Results){
+
+res_mat = dune |> scale(scale = TRUE, center = TRUE)
+
+# the RDA models
+model = rda(res_mat ~ 
+      dune.env$A1 + 
+      dune.env$Moisture + 
+      dune.env$Use + 
+      dune.env$Management + 
+      dune.env$Manure
+      )
+
+eig_val = model$CCA$eig / sum(model$CCA$eig)
+
+score = scores(model, display = "site")
+
+anova.cca(model)
+
+env_vector = function(rda_model){
   env_vector = 
-    vegan::scores(PCA_Results, display = "bp")
-  
-  colnames(env_vector) = c("xend", "yend")  # 環境ベクトルの列名を設定
+    vegan::scores(rda_model, display = "bp", choices = 1:length(rda_model$CCA$eig))
   
   env_vector = env_vector |> 
     as_tibble() |> 
@@ -17,4 +33,18 @@ env_vector = function(PCA_Results){
   
   return(env_vector)
 }
+
+eig_val = env_vector(model)
+
+ggplot() + 
+  geom_point(
+    aes(x = RDA1, y = RDA2, colour = dune.env$Use),
+    data = score
+  ) + 
+  geom_segment(
+    aes(x = 0, xend = RDA1 * 2, y = 0, yend = RDA2 * 2),
+    data = eig_val,
+    arrow = arrow(length = unit(2, "mm"))
+  )
+
 
